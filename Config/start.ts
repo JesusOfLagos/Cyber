36d8cf4963f5dd52c2b7865cfa4587d63effe9c8
Jesus Akanle <jesuswrites20043@gmail.com>
2023-10-26 23:24:57 +0100

    Auth Parse

--- Config/start.ts
+++ Config/start.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response, NextFunction, Express } from 'express';
-import { UserRouter } from '../Routers/User/user.route';
-import { corsOptions } from './cors';
-import cors from 'cors';
+import express, { Request, Response, NextFunction, Express } from "express";
+import { UserRouter } from "../Routers/User/user.route";
+import { corsOptions } from "./cors";
+import cors from "cors";
 
 export default class App {
     public app: Express;
@@ -17,9 +17,9 @@ export default class App {
 
     private initializeMiddlewares(): void {
         this.app.use(express.json());
-        this.app.use(new UserRouter().router)
+        this.app.use(new UserRouter().router);
         this.app.use(express.urlencoded({ extended: true }));
-        this.app.use(cors)
+        this.app.use(cors(corsOptions));
     }
 
     private initializeControllers(controllers: any): void {
